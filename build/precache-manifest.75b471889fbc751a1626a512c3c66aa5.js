93b1056343f552b3bb1577d761c34fa2cd6b42bf
Yahub <43549299+r34son@users.noreply.github.com>
2019-08-11 20:55:33 +0300

    Add files via upload

---
+++ build/precache-manifest.75b471889fbc751a1626a512c3c66aa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1b04756b4713dd7bf7b727629ea805",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fe3069eb597aad4e20f",
+    "url": "/static/css/main.1da519a1.chunk.css"
+  },
+  {
+    "revision": "d818262d8223bac4f29b",
+    "url": "/static/js/2.1711a6f9.chunk.js"
+  },
+  {
+    "revision": "2fe3069eb597aad4e20f",
+    "url": "/static/js/main.10102ef2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
